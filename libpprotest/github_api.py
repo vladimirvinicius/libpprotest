121e665b3b43e209414c494c37865e2ba40dbc22
vladimirvinicius <vladimirvinicius@gmail.com>
2020-09-18 00:33:28 -0300

    Adicionado flake8 e separado dependências
    close #1

---
+++ libpprotest/github_api.py
@@ -0,0 +1,13 @@
+import requests
+
+
+def buscar_avatar(usuario):
+    """
+    Busca o avatar de um usuário no GitHub
+
+    :param usuario: str com o nome de usuário no GitHub
+    :return: str com o link do avatar
+    """
+    url = f'https://api.github.com/users/{usuario}'
+    resp = requests.get(url)
+    return resp.json()['avatar_url']
